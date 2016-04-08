697979c5666feb104e565ef5295704668c77db86
Remco-Ruttenberg <remco.ruttenberg@outlook.com>
2016-04-08 12:04:44 +0200

    Code now follows C++ standard

--- source/include/LocatedDistanceSensor.hpp
+++ source/include/LocatedDistanceSensor.hpp
@@ -4,13 +4,15 @@
 #include "DistanceSensor.hpp"
 #include "Locatable.hpp"
 
-class LocatedDistanceSensor : public DistanceSensor, public Locatable {
-	
-public:
-	LocatedDistanceSensor(double error, CoordinateBearing coordinate_bearing) :
-		DistanceSensor{ error },
-		Locatable{ coordinate_bearing } {}
+namespace R2D2 {
+   class LocatedDistanceSensor : public DistanceSensor, public Locatable {
 
-};
+   public:
+      LocatedDistanceSensor(double error, CoordinateBearing coordinate_bearing) :
+         DistanceSensor{ error },
+         Locatable{ coordinate_bearing } {}
+
+   };
+}
 
 #endif //_LOCATED_DISTANCE_SENSOR_H
--- source/include/Sensor.hpp
+++ source/include/Sensor.hpp
@@ -1,28 +1,32 @@
 #ifndef _SENSOR_H
 #define _SENSOR_H
 
-template<class T>
-class Sensor {
-	
-public:
-	Sensor(double factor) : error_factor{ factor }
-	virtual SensorResult get_data() = 0;
-	
-	class SensorResult<T> {
-		SensorResult() error_factor { 0.0f }, value{}
-		SensorResult(double error_factor, T value) : error_factor{ error_factor }, value{ value } {}
+namespace R2D2 {
+   template<class T>
+   class Sensor {
 
-		double get_error_factor() { return error_factor; }
-		T get_value() { return value; }
+   public:
+      Sensor(double factor) : error_factor{ factor }
+      virtual SensorResult get_data() = 0;
 
-	private:
-		double error_factor;
-		T value;
-	}
+      class SensorResult<T> {
+         SensorResult() error_factor { 0.0f }, value{}
+            SensorResult(double error_factor, T value) :
+            error_factor{ error_factor },
+            value{ value } {}
 
-private:
-	double error_factor;
+         double get_error_factor() { return error_factor; }
+         T get_value() { return value; }
 
-};
+      private:
+         double error_factor;
+         T value;
+      }
+
+   private:
+      double error_factor;
+
+   };
+}
 
 #endif //_SENSOR_H
