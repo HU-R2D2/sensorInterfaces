91f7a228104f6ea48ba7b73c68bdaaf72628c3ce
Stephan Vivie <stephan.vivie@studnet.hu.nl>
2016-04-19 15:50:57 +0200

    Added init and output pin

--- source/src/HwlibTestCode.cpp
+++ source/src/HwlibTestCode.cpp
@@ -35,9 +35,14 @@ extern "C" {
 }
 
 int main() {
-    int pin1 = 11;
+    int pin1 = 0;
     // const int pin2 = 13;
-    
+    if(!bcm2835_init())
+    {
+        // error something wrong initializing
+    }
+
+    bcm2835_gpio_fsel(pin1, BCM2835_GPIO_FSEL_OUTP)
     hwlib_blink_pin_ms(pin1, 500);
     
     return 0;
