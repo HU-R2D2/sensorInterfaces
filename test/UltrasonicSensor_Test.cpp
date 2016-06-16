f385c769ea75578d6a923f628d71f88dbf0759da
Remco-Ruttenberg <remco.ruttenberg@outlook.com>
2016-06-16 17:07:54 +0200

    Fixed gtests failing due to variance of actual sleep time

--- test/UltrasonicSensor_Test.cpp
+++ test/UltrasonicSensor_Test.cpp
@@ -1,22 +1,23 @@
 #include "gtest/gtest.h"
 #include "../source/include/UltrasonicSensor.hpp"
-#include <chrono>
-#include <thread>
 
 using namespace r2d2;
 
 TEST(UltrasonicSensor, LinearMeasurements) {
     UltrasonicSensor uss{ 0.1, CoordinateAttitude{},0,0 };
     // Needed as the last time is set when constructing the object
-    std::this_thread::sleep_for(std::chrono::microseconds(210));
+	 TimeStamp ts = Clock::get_current_time();
+	 while (Clock::get_current_time() - ts < 210 * Duration::MICROSECOND);
     Length l = uss.get_distance();
+	 ts = Clock::get_current_time();
     ASSERT_FALSE(l > 1 * Length::CENTIMETER);
     ASSERT_FALSE(l < 1 * Length::CENTIMETER);
-    std::this_thread::sleep_for(std::chrono::microseconds(210));
+	 while (Clock::get_current_time() - ts < 210 * Duration::MICROSECOND);
     l = uss.get_distance();
+	 ts = Clock::get_current_time();
     ASSERT_FALSE(l > 2 * Length::CENTIMETER);
     ASSERT_FALSE(l < 2 * Length::CENTIMETER);
-    std::this_thread::sleep_for(std::chrono::microseconds(210));
+	 while (Clock::get_current_time() - ts < 210 * Duration::MICROSECOND);
     l = uss.get_distance();
     ASSERT_FALSE(l > 3 * Length::CENTIMETER);
     ASSERT_FALSE(l < 3 * Length::CENTIMETER);
@@ -25,8 +26,10 @@ TEST(UltrasonicSensor, LinearMeasurements) {
 TEST(UltrasonicSensor, TooFastMeasuring) {
     UltrasonicSensor uss{ 0.1, CoordinateAttitude{},0,0 };
     // Needed as the last time is set when constructing the object
-    std::this_thread::sleep_for(std::chrono::microseconds(210));
+	 TimeStamp ts = Clock::get_current_time();
+	 while (Clock::get_current_time() - ts < 210 * Duration::MICROSECOND);
     Length l = uss.get_distance();
+	 // Don't wait to trigger reading exception
     ASSERT_FALSE(l > 4 * Length::CENTIMETER);
     ASSERT_FALSE(l < 4 * Length::CENTIMETER);
     ASSERT_THROW(uss.get_distance(), ReadingFailedException);
